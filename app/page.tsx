159664fc108571997cc62a10b6c67497e5803823
jkkim09 <41351293+jkkim09@users.noreply.github.com>
2025-08-29 10:41:46 +0900

    init: 초기 세팅

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+const Page = () => {
+  return (
+    <div className="">
+      <h1 className="text-large">TEST</h1>
+      <h1
+        style={{
+          fontSize: "48px",
+        }}
+      >
+        TEST2
+      </h1>
+      <div className="rounded-5xl border-primary max-h-[100px]"></div>
+    </div>
+  );
+};
+
+export default Page;
---
+++ app/test/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const TestPage = () => {
+  return (
+    <div>
+      <h1 className="text-7xl  text-primary">TEST PAGE</h1>
+    </div>
+  );
+};
+
+export default TestPage;
