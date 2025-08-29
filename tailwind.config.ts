135bc1d305cc3581f3c58765857801cd21b4ce35
jkkim09 <41351293+jkkim09@users.noreply.github.com>
2025-08-29 10:48:28 +0900

    update: 초기 설정 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,12 +3,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontSize: {
-        xxs: "0.625rem",
-        huge: "4rem",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
