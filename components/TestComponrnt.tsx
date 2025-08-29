b48b1228ccf371d380555bc7114756605b778f5a
jkkim09 <41351293+jkkim09@users.noreply.github.com>
2025-08-29 11:01:08 +0900

    update: scss 적용

--- components/TestComponrnt.tsx
+++ components/TestComponrnt.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 const TestComponent = () => {
-  return <div className="w-[200px] h-[50px] bg-primary text-large">TEST</div>;
+  return (
+    <div className="w-[200px] h-[50px] bg-primary text-large">
+      <div className="testStyle">TEST</div>
+    </div>
+  );
 };
 
 export default TestComponent;
