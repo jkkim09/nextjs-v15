144e1e553be15236ee74d861406766ca709a0cbd
jkkim09 <41351293+jkkim09@users.noreply.github.com>
2025-09-04 17:22:57 +0900

    update: font 다시 원복

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,20 @@
 
 import './globals.css';
 import { Provider } from 'react-redux';
+import { Geist, Geist_Mono } from 'next/font/google';
 import store from '@/stores/store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Providers from './providers';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
 
 const queryClient = new QueryClient();
 
@@ -17,7 +28,9 @@ const RootLayout = ({
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <html lang="ko" suppressHydrationWarning>
-          <body>
+          <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          >
             <Providers>
               <main>{children}</main>
             </Providers>
