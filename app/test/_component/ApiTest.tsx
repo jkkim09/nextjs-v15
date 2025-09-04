dcaca46ae51e6b832e04d51d28e1b4c874a586d4
jkkim09 <41351293+jkkim09@users.noreply.github.com>
2025-09-04 15:39:04 +0900

    update: ssr+csr test code

--- app/test/_component/ApiTest.tsx
+++ app/test/_component/ApiTest.tsx
@@ -9,34 +9,32 @@ interface Item {
   userId: number;
 }
 
-interface ITest {
+export interface ITest {
   limit: number;
   skip: number;
   total: number;
   todos: Item[];
 }
 
-const ApiTest = ({ items }: { items: Item[] }) => {
+const ApiTest = ({ item }: { item: ITest }) => {
   /**
    *  SSR 에서 받아온 값을 initialData 초기화 하여 SSR렌더링 시킨다.
    */
   const { isPending, data } = useQuery<ITest>({
     queryKey: ['todos'],
-    initialData: {
-      limit: 10,
-      skip: 10,
-      total: 10,
-      todos: items,
-    },
+    initialData: item,
     queryFn: () =>
       fetch('https://dummyjson.com/todos').then((res) => res.json()),
+    refetchOnMount: true, // 페이지 다시 마운트 시 자동 refetch
   });
 
   return (
     <div>
       <h1 className="text-7xl  text-primary">TEST PAGE</h1>
       <h2>{isPending ? 'isPending' : 'noIsPending'}</h2>
-      <h3>{data?.total}</h3>
+      <h3>
+        {data?.total}:{data?.limit}:{data?.skip}
+      </h3>
       <ul>
         {data?.todos.map((item, index) => {
           return <li key={index}>{item.todo}</li>;
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,31 +1,21 @@
-import ApiTest from './_component/ApiTest';
+import ApiTest, { ITest } from './_component/ApiTest';
 
-const TestPage = () => {
+const TestPage = async () => {
+  const data: ITest = await fetch('https://dummyjson.com/todos').then((res) =>
+    res.json()
+  );
+  console.log('SSR ---', data);
   return (
     <div>
       <h1>TEST PAGE TEST PAGE</h1>
       <h2>React-Query</h2>
       <ApiTest
-        items={[
-          {
-            id: 1,
-            todo: 'Do something nice for someone you care about',
-            completed: false,
-            userId: 152,
+        item={{
+          ...data,
+          ...{
+            total: 1000000000,
           },
-          {
-            id: 2,
-            todo: 'Memorize a poem',
-            completed: true,
-            userId: 13,
-          },
-          {
-            id: 3,
-            todo: 'Watch a classic movie',
-            completed: true,
-            userId: 68,
-          },
-        ]}
+        }}
       />
       <h3>TEST 333333</h3>
     </div>
